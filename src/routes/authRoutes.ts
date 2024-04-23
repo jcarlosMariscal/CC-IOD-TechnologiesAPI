c069d75d825264a1171551206996112d57d01ea5
JCarlosrm <juanc.ramirezm16@gmail.com>
2024-04-23 12:50:48 -0600

    Inicio de sesión

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,8 @@
 import express from "express";
-import { register } from "../controllers/authController";
-// import { login, register } from "../controllers/authController";
-
+import { login, register } from "../controllers/authController";
 const router = express.Router();
 
 router.post("/register", register);
-// router.post("/login", login);
+router.post("/login", login);
 
 export default router;
