0d84f707add7f5401e6c9a25123ae60caea3f706
JCarlosrm <juanc.ramirezm16@gmail.com>
2024-04-23 21:33:15 -0600

    Middleware para validar Prospecto con Joi

--- src/controllers/prospectController.ts
+++ src/controllers/prospectController.ts
@@ -2,26 +2,27 @@ import { Request, Response } from "express";
 import { pool } from "../database/connection";
 
 export const createProspect = async (req: Request, res: Response): Promise<Response> => {
-  const { name, email, phone, relationshipId, stateId, date, observations } = req.body;
+  const { name, email, phone, relationshipId, statusId, date, observations } = req.body;
   try {
-    if (!name) return res.status(400).json({ error: "El nombre es obligatorio" });
-    if (!email) return res.status(400).json({ error: "El correo es obligatorio" });
-    if (!phone) return res.status(400).json({ error: "El teléfono es obligatorio" });
-    if (!relationshipId) return res.status(400).json({error: "El paréntesco es obligatorio" });
-    if (!stateId) return res.status(400).json({ error: "El estado es obligatorio" });
-    if (!date) return res.status(400).json({ error: "La fecha es obligatorio" });
     const optionalData = observations ? observations : ""
 
     const query = {
-      text: 'INSERT INTO prospects(name, email, phone, date, relationshipId, stateId, observations) VALUES($1, $2, $3, $4, $5, $6, $7)',
-      values: [name, email, phone, date, relationshipId, stateId, optionalData],
+      text: 'INSERT INTO prospects(name, email, phone, date, relationshipId, statusId, observations) VALUES($1, $2, $3, $4, $5, $6, $7)',
+      values: [name, email, phone, date, relationshipId, statusId, optionalData],
     }
     await pool.query(query);
     return res.status(201).json({
       message: 'El prospecto se ha creado correctamente',
       prospect: { name, email }
     });
-  } catch (error) {
+  } catch (error:any) {
+    if (error?.code === "22007") return res.status(400).json({ message: "Verifique que la fecha sea correcta" });
+    if (error?.code === "23503" && error.constraint.includes("relationshipid")) return res.status(400).json(
+      { message: "Parece que no existe el parentesco seleccionado. Seleccione una correcta" }
+    );
+    if (error?.code === "23503" && error.constraint.includes("status")) return res.status(400).json(
+      { message: "Parece que no existe el estado seleccionado. Seleccione una correcta" }
+    );
     return res.status(500).json(
       { message: "Ha ocurrido un error en el servidor. Intente de nuevo más tarde", error }
     );
--- src/routes/prospectsRoutes.ts
+++ src/routes/prospectsRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { createProspect } from "../controllers/prospectController";
 import { authenticateToken } from "../middlewares/authenticateToken";
+import { validateProspect } from "../middlewares/validationsProspects";
 
 const router = express.Router();
 
 router.use(authenticateToken);
-router.post("/", createProspect);
+router.post("/", validateProspect, createProspect);
 
 export default router;
