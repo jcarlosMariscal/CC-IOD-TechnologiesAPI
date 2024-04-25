c0e17344fa1e72048026a13ea32c88b206c7cc3d
JCarlosrm <juanc.ramirezm16@gmail.com>
2024-04-24 21:02:54 -0600

    Ajustes a obtener prospecto por id | Consultas con INNER JOIN | Editar y eliminar prospecto

--- src/routes/prospectsRoutes.ts
+++ src/routes/prospectsRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProspect, getAllProspects } from "../controllers/prospectController";
+import { createProspect, deleteProspect, getAllProspects, getProspectById, updateProspect } from "../controllers/prospectController";
 import { authenticateToken } from "../middlewares/authenticateToken";
 import { validateProspect } from "../middlewares/validationsProspects";
 
@@ -8,5 +8,8 @@ const router = express.Router();
 router.use(authenticateToken);
 router.get("/", getAllProspects);
 router.post("/", validateProspect, createProspect);
+router.get("/:id", getProspectById);
+router.put("/:id", validateProspect, updateProspect);
+router.delete("/:id", deleteProspect);
 
 export default router;
