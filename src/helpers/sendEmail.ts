03df76e1471ad63918140c3698fbe1f9a498a225
JCarlosrm <juanc.ramirezm16@gmail.com>
2024-06-05 09:43:09 -0600

    Omitir imágen en el correo

--- src/helpers/sendEmail.ts
+++ src/helpers/sendEmail.ts
@@ -19,14 +19,15 @@ export const sendEmail = (
   name: string,
   token?: string
 ) => {
-  let content = `<html lang="es">
-        <head>
-          <title>Reset Password</title>
-        </head>
-        <body>
-        <div style="text-align:center;background:#192c7b;">
-          <img src="cid:uniqueImageCID" alt="Logo CCIOD Technologies" />
-        </div>
+  // <div style="text-align:center;background:#192c7b;">
+  //   <img src="cid:uniqueImageCID" alt="Logo CCIOD Technologies" />
+  // </div>;
+  let content = `
+    <html lang="es">
+      <head>
+        <title>Reset Password</title>
+      </head>
+      <body>
         <div style="color: #121519; text-align:justify;">`;
   content += `<p>Hola, <span style="font-weight: bold;">${name}</span>, `;
   content += token
@@ -35,13 +36,13 @@ export const sendEmail = (
   content += `<hr style="opacity: .20; margin: 1rem 0;" />`;
   content += token
     ? `<p>Haz clic en el botón de abajo y crea una nueva contraseña. Si no solicitaste restablecer tu contraseña, ignora este correo electrónico. En caso de que hayas recibido más de un correo, debes utilizar el último que te llegó.</p>
-          <div style="text-align:center; margin-top: 2rem;">
-          <a href="${process.env.FRONTEND_URL}reset-password/${token}" style="background: #192c7b; color: #eee; margin: 1rem 0;padding: .8rem; border-radius: .5rem; cursor: pointer; text-decoration:none;">Reestablecer contraseña</a>
-          </div>`
+      <div style="text-align:center; margin-top: 2rem;">
+        <a href="${process.env.FRONTEND_URL}reset-password/${token}" style="background: #192c7b; color: #eee; margin: 1rem 0;padding: .8rem; border-radius: .5rem; cursor: pointer; text-decoration:none;">Reestablecer contraseña</a>
+      </div>`
     : `<p>Ahora puede iniciar sesión con su nueva contraseña.</p>
           <div style="text-align:center; margin-top: 2rem;">
-          <a href="${process.env.FRONTEND_URL}" style="background: #192c7b; color: #eee; margin: 1rem 0;padding: .8rem; border-radius: .5rem; cursor: pointer; text-decoration:none;">Iniciar sesión</a>
-          </div>`;
+        <a href="${process.env.FRONTEND_URL}" style="background: #192c7b; color: #eee; margin: 1rem 0;padding: .8rem; border-radius: .5rem; cursor: pointer; text-decoration:none;">Iniciar sesión</a>
+      </div>`;
   content += `<hr style="opacity: .20; margin: 1rem 0;" />
           <div style="text-align:center;">
           <span>© 2024 CCIOD,  Todos los derechos reservados.</span>
@@ -54,13 +55,13 @@ export const sendEmail = (
     to,
     subject,
     html: content,
-    attachments: [
-      {
-        filename: "cciod-technologies.png",
-        path: __dirname + "../../assets/cciod-technologies.png",
-        cid: "uniqueImageCID", // Referenced in the HTML template
-      },
-    ],
+    // attachments: [
+    //   {
+    //     filename: "cciod-technologies.png",
+    //     path: __dirname + "../../assets/cciod-technologies.png",
+    //     cid: "uniqueImageCID", // Referenced in the HTML template
+    //   },
+    // ],
   };
   return new Promise((resolve, reject) => {
     transporter.sendMail(mailOptions, (err, response) => {
