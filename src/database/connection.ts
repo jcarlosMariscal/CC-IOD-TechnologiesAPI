7264ac745a4ea58966c0a77a129a3e5615d17490
JCarlosrm <juanc.ramirezm16@gmail.com>
2024-06-21 14:23:00 -0600

    SSL production or development

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,9 @@
 import pkg from "pg";
 
 const { Pool } = pkg;
+const isProduction = process.env.NODE_ENV === "production";
 
 export const pool = new Pool({
   allowExitOnIdle: true,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
