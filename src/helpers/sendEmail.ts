46bcd8bee500705ee994c113b1015655a6e01121
JCarlosrm <juanc.ramirezm16@gmail.com>
2024-07-01 19:45:50 -0600

    Cambio en correo de reestablecimiento

--- src/helpers/sendEmail.ts
+++ src/helpers/sendEmail.ts
@@ -17,6 +17,7 @@ export const sendEmail = (
   name: string,
   token?: string
 ) => {
+  const currentYear = new Date().getFullYear();
   let html = `
     <html lang="es">
       <head>
@@ -24,13 +25,14 @@ export const sendEmail = (
       </head>
       <body>
         <div style="color: #121519; text-align:justify;">`;
-  html += `<p>Hola, <span style="font-weight: bold;">${name}</span>, `;
+  html += `<p>Hola <span style="font-weight: bold;">${name}</span>, `;
   html += token
     ? `hemos recibido una petición para reestablecer su contraseña.</p>`
     : `su contraseña se ha reestablecido.</p>`;
   html += `<hr style="opacity: .20; margin: 1rem 0;" />`;
   html += token
     ? `<p>Haz clic en el botón de abajo y crea una nueva contraseña. Si no solicitaste restablecer tu contraseña, ignora este correo electrónico. En caso de que hayas recibido más de un correo, debes utilizar el último que te llegó.</p>
+    <p>Este enlace dejará de ser útil en las próximas 4 horas. Por favor, asegúrate de utilizarlo antes de que expire.</p>
       <div style="text-align:center; margin-top: 2rem;">
         <a href="${process.env.FRONTEND_URL}reset-password/${token}" style="background: #192c7b; color: #eee; margin: 1rem 0;padding: .8rem; border-radius: .5rem; cursor: pointer; text-decoration:none;">Reestablecer contraseña</a>
       </div>`
@@ -40,7 +42,7 @@ export const sendEmail = (
       </div>`;
   html += `<hr style="opacity: .20; margin: 1rem 0;" />
           <div style="text-align:center;">
-          <span>© 2024 CCIOD,  Todos los derechos reservados.</span>
+          <span>© ${currentYear} CCIOD Technologies,  Todos los derechos reservados.</span>
           </div>
         </div>
         </body>
