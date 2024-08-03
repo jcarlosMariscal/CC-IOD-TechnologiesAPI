6a92bdaa720fd9ee59564453c000ee378d45e0eb
JCarlosrm <juanc.ramirezm16@gmail.com>
2024-08-02 19:37:01 -0600

    Actualización origenes permitidos para CORS

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 const isProduction = process.env.NODE_ENV === "production";
 
 if (isProduction) {
-  const allowedOrigins = ["https://cciodtech.com", "https://dev.cciodtech.com"];
+  const allowedOrigins = ["https://cciodtechnologies.com/"];
   const corsOptions: cors.CorsOptions = {
     origin: (origin, callback) => {
       if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
